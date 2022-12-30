ee64eab8d65f1d669f1fe4bcec77b1f0d25730a7
Jean-Charles ALLA <charlister22@mail.com>
2022-12-30 19:29:37 +0100

    suppression du fichier weather_icone.dart

--- lib/exo2/page/weather_page.dart
+++ lib/exo2/page/weather_page.dart
@@ -6,7 +6,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:tp2/exo2/cubit/weather_cubit.dart';
 import 'package:tp2/exo2/model/weather_model.dart';
-import 'package:tp2/exo2/widget/weather_icone.dart';
 
 class WeatherPage extends StatelessWidget {
   late TextField searchField;
@@ -317,5 +316,27 @@ class WeatherPage extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  Widget getWeatherIcon({required String weatherDescription, required Color color, required double size}) {
+    Icon result;
+    switch(weatherDescription) {
+      case "Clear":
+        result = Icon(FontAwesomeIcons.sun, color: color, size: size,);
+        break;
+      case "Clouds":
+        result = Icon(FontAwesomeIcons.cloud, color: color, size: size,);
+        break;
+      case "Rain":
+        result = Icon(FontAwesomeIcons.cloudRain, color: color, size: size,);
+        break;
+      case "Snow":
+        result = Icon(FontAwesomeIcons.snowman, color: color, size: size,);
+        break;
+      default:
+        result = Icon(FontAwesomeIcons.sun, color: color, size: size,);
+        break;
+    }
+    return result;
   }
 }
